bc2344c9a320f4009d7df8ce61f1b5d5f952fe8b
DOGHOUSETV <arubigeorge0@gmail.com>
2025-01-01 18:54:48 -0500

    Responsive navigation bar with logos

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -2,7 +2,25 @@ import React from 'react';
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <nav className="w-full p-4">
+      <div className="container mx-auto px-4 py-2 flex justify-between items-center">
+        <img 
+          src="/contact_logo.svg"
+          alt="Contact Logo"
+          className="h-6 w-auto"
+        />
+        <img 
+          src="/patchbay_logo.png"
+          alt="Patchbay Logo"
+          className="h-6 w-auto"
+        />
+        <img 
+          src="/account_logo.svg"
+          alt="Account Logo"
+          className="h-8 w-auto"
+        />
+      </div>
+    </nav>
   );
 };
 
