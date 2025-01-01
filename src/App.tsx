d973db1acb6c7a25c0d8578b877d7a47d642f5e2
DOGHOUSETV <arubigeorge0@gmail.com>
2025-01-01 18:08:47 -0500

    Initial app structure with nav, file upload, and contract form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import NavBar from './components/navbar';
+import FileUpload from 'components/file-upload';
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
+      <FileUpload />
     </div>
   );
 }
---
+++ src/components/contract-form/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ContractForm = () => {
+  return (
+    <div>ContractForm</div>
+  );
+};
+
+export default ContractForm;
